b585cbdd447ac18374635d0d47e3eb8a4eee2d18
gj1342 <38597761+gj1342@users.noreply.github.com>
2025-03-18 12:17:50 +0800

    1 -project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+    res.json({ data: "signup route" });
+}
+
+export const login = async (req, res) => {
+    res.json({ data: "login route" });
+}
+
+export const logout = async (req, res) => {
+    res.json({ data: "logout route" });
+}
