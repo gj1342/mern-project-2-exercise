325f8e70fea3278f17212c8b0c99cf839b9af83a
gj1342 <38597761+gj1342@users.noreply.github.com>
2025-03-23 00:22:38 +0800

    3 - updated post controller for like functionality

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -106,7 +106,9 @@ export const likeUnlikePost = async (req, res) => {
             await Post.updateOne({_id:postId}, {$pull: {likes: userId}});
             await User.updateOne({_id: userId}, {$pull: {likePosts: postId}});
 
-            res.status(200).json({message: "Post unliked successfully"});
+            const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString());
+
+            res.status(200).json(updatedLikes);
         } else {
             post.likes.push(userId);
             await User.updateOne({_id: userId}, {$push: {likePosts: postId}});
@@ -120,7 +122,8 @@ export const likeUnlikePost = async (req, res) => {
             
             await notification.save();
 
-            res.status(200).json({message: "Post liked successfully"});
+            const updatedLikes = post.likes;
+            res.status(200).json(updatedLikes);
         }
         
         
